fc3739c269dfd4f08d974e776b7826227b6d23e5
sebadesario95-rgb <sebadesario95@gmail.com>
2026-09-23 20:52:13 +0200

    Update SeriesScreen.kt

--- app/src/main/java/com/example/xtreamplayer/ui/SeriesScreen.kt
+++ app/src/main/java/com/example/xtreamplayer/ui/SeriesScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
